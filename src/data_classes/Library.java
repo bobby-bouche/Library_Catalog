528597a7d1ab090f53e940408d8140989b141d8b
bobby-bouche <n_8-bit_Bobby@hotmail.com>
2025-10-02 21:51:35 -0700

    added addItem methoed to library class

--- src/data_classes/Library.java
+++ src/data_classes/Library.java
@@ -6,17 +6,24 @@ public class Library {
 	
 	// fields
 	private ArrayList<Integer> libraryAccounts;
+	private Catalog<Book> bookCatalog;
 
 	
 	// constructor
 	public Library() {
 		super();
 		libraryAccounts = new ArrayList<>();
+		bookCatalog     = new Catalog<>();
 	}
 
 
 	public ArrayList<Integer> getLibraryAccounts() {
 		return libraryAccounts;
+	}
+
+
+	public Catalog<Book> getBookCatalog() {
+		return bookCatalog;
 	}
 	
 }
